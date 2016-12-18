cb422e9bd4db238562ded7fedd9c2dbea427d375
Joshua Webb <bbew.auhsoj+git@gmail.com>
2016-12-18 18:54:29 +1100

    Compatibility attempt #1
     - Try some more config values found sprinkled around the net

--- app/src/main/java/io/github/joshuawebb/batterynotificationmod/DisableLowBatteryNotifications.java
+++ app/src/main/java/io/github/joshuawebb/batterynotificationmod/DisableLowBatteryNotifications.java
@@ -1,6 +1,13 @@
 package io.github.joshuawebb.batterynotificationmod;
 
+import android.content.res.Resources;
+import android.text.TextUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import de.robv.android.xposed.IXposedHookInitPackageResources;
+import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.callbacks.XC_InitPackageResources;
 
 public class DisableLowBatteryNotifications implements IXposedHookInitPackageResources {
@@ -13,7 +20,43 @@ public class DisableLowBatteryNotifications implements IXposedHookInitPackageRes
 		if (!resparam.packageName.equals(targetPackage))
 			return;
 
-		resparam.res.setReplacement(targetPackage, "integer", "config_staminaWarningLevel", -1);
-		resparam.res.setReplacement(targetPackage, "integer", "config_staminaCriticalLevel", -1);
+		String[] configs = new String[] {
+			// Xperia devices
+			"config_staminaWarningLevel",
+			"config_staminaCriticalLevel",
+
+			// Other devices
+			"config_lowBatteryCloseWarningLevel",
+			"config_lowBatteryWarningLevel",
+			"config_criticalBatteryWarningLevel"
+		};
+
+		List<String> set = new ArrayList<>();
+		List<String> notSet = new ArrayList<>();
+		for(String config : configs) {
+			if (setReplacement(resparam, config, -1)) {
+				set.add(config);
+			} else {
+				notSet.add(config);
+			}
+		}
+
+		XposedBridge.log("BatteryMod - set: " + TextUtils.join(", ", set));
+		XposedBridge.log("BatteryMod - didn't exist: " + TextUtils.join(", ", notSet));
+	}
+
+	private boolean setReplacement(
+			XC_InitPackageResources.InitPackageResourcesParam resparam,
+			String config,
+			int value)
+	{
+		try {
+			resparam.res.setReplacement(targetPackage, "integer", config, value);
+			return true;
+		}
+		catch (Resources.NotFoundException nfe)
+		{
+			return false;
+		}
 	}
 }
